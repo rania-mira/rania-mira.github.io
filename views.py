0bb31d1725c00c696d1abc5cd1199470bf264955
Rania <103965163+rania-mira@users.noreply.github.com>
2022-04-25 14:24:45 +0200

    Add files via upload

--- views.py
+++ views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse 
-
-# Create your views here.
-def say_hello(request):
-    return HttpResponse('hello world')
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse('<h1 style='text-align:center'>website django</h1>')
